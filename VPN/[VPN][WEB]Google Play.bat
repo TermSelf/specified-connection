d8881d6525f160bcb0c30f174983e8fb76d56ca8
TermSelf <a34021501a@gmail.com>
2018-04-12 03:38:12 +0800

    Update [VPN][WEB]Google Play.bat

--- VPN/[VPN][WEB]Google Play.bat
+++ VPN/[VPN][WEB]Google Play.bat
@@ -26,7 +26,7 @@ if '%errorlevel%' NEQ '0' (
 
 
 REM Made by TermSelf
-REM Version 2.0_2018_April_4_VPN
+REM Version 2.0_2018_April_12_VPN
 :-------------------------------------
 
 
@@ -104,6 +104,7 @@ for /l %%i in (1,1,4) do (
 >nul 2>&1 route delete !ip_%%i!
 >nul 2>&1 route add !ip_%%i! mask %mask_3% %gateway% if %if% metric 1
 )
+endlocal
 setlocal DisableDelayedExpansion
 
 goto Connected
@@ -123,6 +124,7 @@ setlocal EnableDelayedExpansion
 for /l %%i in (1,1,4) do (
 >nul 2>&1 route delete !ip_%%i!
 )
+endlocal
 setlocal DisableDelayedExpansion
 
 goto Connected
